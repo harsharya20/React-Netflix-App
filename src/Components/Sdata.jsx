cf00257ce0c862df718094cd0a22d48f38aa0a7b
Harsh <harsharya7002@gmail.com>
2022-04-04 16:21:58 +0530

    Playing with components

---
+++ src/Components/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata = [
+    {
+      sname:'DARK',
+      imgsrc: 'https://wallpapercave.com/w/wp4056399.jpg',
+      title: 'A Netflix Original Series',
+      link: 'https://www.netflix.com/in/title/80100172'
+    },
+    {
+        sname:'Stranger Things',
+        imgsrc: 'https://wallpapercave.com/w/wp10780309.jpg',
+        title: 'A Netflix Original Series',
+        link: 'https://www.netflix.com/in/title/80057281'
+    },
+    {
+        sname:'Daredevil',
+      imgsrc: 'https://wallpapercave.com/w/wp3920147.jpg',
+      title: 'A Netflix Original Series',
+      link: 'https://www.netflix.com/in/title/80018294'
+    },
+    {
+        sname:'Breaking Bad',
+        imgsrc: 'https://wallpapercave.com/w/wp2156334.jpg',
+        title: 'Crime Drama Television Series',
+        link: 'https://www.netflix.com/in/title/70143836'
+    },
+    {
+        sname:'Never Have I Ever',
+        imgsrc: 'https://wallpapercave.com/w/wp6196648.jpg',
+        title: 'A Netflix Original Series',
+        link: 'https://www.netflix.com/in/title/80179190'
+    }
+]
+
+export default Sdata
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Cards from './Components/Cards'
+import './index.css'
+import Sdata from './Components/Sdata'
+
+// const ncard = (val) => {
+//   return(
+// <Card 
+// imgsrc = {val.imgsrc} 
+// title={val.title}
+// sname={val.sname}
+// link={val.link}
+// />
+//   )
+// }
+
+ReactDOM.render(
+  <>
+  <h1 className='heading_style'>List of Top 5 Netflix Series</h1>
+{Sdata.map((val) => {
+  return(
+    <Cards 
+    imgsrc = {val.imgsrc} 
+    title={val.title}
+    sname={val.sname}
+    link={val.link}
+    />
+  )
+})}
+</>,
+document.getElementById('root')
+)
